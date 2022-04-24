844421aa3c779f6e567aa2f49b838e9ad8f5cb4c
grazia <grazia.ferrara2000@gmail.com>
2022-04-24 11:05:58 +0200

    Initial Commit

---
+++ tirocinio/tirocinioApp.swift
@@ -0,0 +1,17 @@
+//
+//  tirocinioApp.swift
+//  tirocinio
+//
+//  Created by Grazia Ferrara on 24/04/22.
+//
+
+import SwiftUI
+
+@main
+struct tirocinioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
