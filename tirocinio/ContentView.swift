ec366d158cdf2ceff4f772c6f63bcb165720376d
salvatoreA0 <dev@salvatoreapicella.com>
2022-04-25 11:33:50 +0200

    Creati i file dei singoli tab ed implementato il sistema per gestire l'orientamento verticale ed orizzontale dell'applicazione

--- tirocinio/ContentView.swift
+++ tirocinio/ContentView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Grazia!")
-            .padding()
+        
+//        NavigationView{
+//            GeometryReader { geo in
+//                let screenHeight = geo.frame(in: .global).height
+//                let screenWidth = geo.frame(in: .global).width
+//                if screenHeight < screenWidth { //layout orizzontale
+//
+//                } else { //layout verticale
+//
+//                }
+//            }
+//            .navigationTitle("")
+//            .navigationBarHidden(true)
+//        }
+//        .navigationViewStyle(.stack)
+        
+        TabView {
+            ObjectView()
+                .tabItem{
+                    Label("Oggetti", systemImage: "archivebox.fill")
+                    Text("Oggetti")
+                }
+            BagView()
+                .tabItem{
+                    Label("Valigie", systemImage: "suitcase.fill")
+                    Text("Valigie")
+                }
+            TripView()
+                .tabItem{
+                    Label("Viaggi", systemImage: "globe.europe.africa.fill")
+                    Text("Viaggi")
+                }
+        }
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
