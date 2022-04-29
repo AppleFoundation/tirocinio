ef2b3145c41bee1cbea012e9012b6920009fd2e3
Cristian Cerasuolo <cristian.cerasuolo2602@gmail.com>
2022-04-29 22:24:31 +0200

    First steps of CoreData

--- tirocinio/tirocinioApp.swift
+++ tirocinio/tirocinioApp.swift
@@ -10,56 +10,13 @@ import CoreData
 
 @main
 struct tirocinioApp: App {
-    let persistence = PersistenceManager()
+    let persistence = PersistenceManager.shared
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //Iniettiamo la variabile d'ambiente rappresentante il contesto all'interno dell'applicazione
+            ContentView().environment(\.managedObjectContext, persistence.persistentContainer.viewContext)
+//            ContentView().environmentObject(persistence)
         }
     }
-}
-
-class PersistenceManager {
-    let persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer (name: "model")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-    
-    init(){
-        let center = NotificationCenter.default
-        let notification = UIApplication.willResignActiveNotification
-        
-        center.addObserver(forName: notification, object: nil, queue: nil) { [weak self] _ in
-            guard let self = self else { return }
-            
-            if self.persistentContainer.viewContext.hasChanges {
-                try? self.persistentContainer.viewContext.save()
-            }
-        }
-    }
-    
-    //     for later usage
-    func saveContext() {
-      // 1
-      let context = persistentContainer.viewContext
-      // 2
-      if context.hasChanges {
-        do {
-          // 3
-          try context.save()
-        } catch {
-          // 4
-          // The context couldn't be saved.
-          // You should add your own error handling here.
-          let nserror = error as NSError
-          fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-        }
-      }
-    }
-
 }
