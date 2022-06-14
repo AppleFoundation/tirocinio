7528e00942123ffe8cec66a1012ab23f3f96c963
Cristian Cerasuolo <cristian.cerasuolo2602@gmail.com>
2022-06-14 19:10:35 +0200

    test

---
+++ tirocinio/Viaggio+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  Viaggio+CoreDataProperties.swift
+//  tirocinio
+//
+//  Created by Cristian Cerasuolo on 11/06/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Viaggio {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Viaggio> {
+        return NSFetchRequest<Viaggio>(entityName: "Viaggio")
+    }
+
+    @NSManaged public var data: Date?
+    @NSManaged public var id: UUID?
+    @NSManaged public var nome: String?
+
+}
+
+extension Viaggio : Identifiable {
+
+}
