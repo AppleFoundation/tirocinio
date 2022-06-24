c56b23bc1c17b197fcaed7cca558f14f3fb342e0
grazia <grazia.ferrara2000@gmail.com>
2022-06-24 02:03:06 +0200

    .

--- SpeechToText/DecodeInput.swift
+++ SpeechToText/DecodeInput.swift
@@ -11,6 +11,7 @@ public class DecodeInput {
     
     public static var oggetti = [(name:String, category:String)]()
     public static var valigie = [(name:String, category:String)]()
+    public static var valigieUsate : [String] = []
     
     public func searchIntoCollection(){
         DecodeInput.oggetti.removeAll()
@@ -60,14 +61,23 @@ public class DecodeInput {
                 // se non sono stati trovati oggetti verifico se si tratta di una valigia da inserire
                 for c in DecodeInput.valigie {
                     if(text.lowercased().contains(c.name.lowercased())){
-                        let valigia = PersistenceManager.shared.loadValigieFromNomeCategoria(nome: c.name, categoria: c.category)[0]
-                        let pesoMassimo = getPesoMassimo(input: text.lowercased(), name: c.name, category: c.category)
-                        print(pesoMassimo)
-                        PersistenceManager.shared.addValigiaViaggiante(valigia: valigia, viaggio: viaggio, pesoMassimo: pesoMassimo)
+                        let rep = contaOccorrenze(input: text.lowercased(), name: c.name.lowercased())
+                        
+                        for i in 1...rep{
+                            let valigia = PersistenceManager.shared.loadValigieFromNomeCategoria(nome: c.name, categoria: c.category)[0]
+
+                            DecodeInput.valigieUsate.append(valigia.nome!)
+                            let pesoMassimo = getPesoMassimo(input: text.lowercased(), name: c.name, category: c.category)
+                            
+                            print("------------------------Peso:\(pesoMassimo)---------------------------")
+                            PersistenceManager.shared.addValigiaViaggiante(valigia: valigia, viaggio: viaggio, pesoMassimo: pesoMassimo)
+                        }
+                        
                         done = true
                     }
                 }
-            
+                
+            DecodeInput.valigieUsate.removeAll()
                 
             case "rimuovi":
                 
@@ -128,27 +138,78 @@ public class DecodeInput {
     // funzione che restituisce il peso massimo della valigia viaggiante da inserire
     public func getPesoMassimo(input: String, name: String, category: String) -> Int {
         
-        var peso: Int
+        var peso = 0
+        var countOcc = 0
+        
+        for conta in DecodeInput.valigieUsate{
+            if(conta == name){
+                countOcc += 1
+            }
+        }
+        
+        print(countOcc)
         
         // cerco il peso della valigia all'interno della stringa inserita
+        // countOcc, se diverso da 0, contiene il numero di occorrenze del nome della valigia da skippare prima di prendere il peso
+        let inputArray = input.components(separatedBy: " ")
         
+        var i = 0, c = 0
+        for item in inputArray {
+            
+            if (item.lowercased() == name.lowercased()){
+                c += 1
+            }
+            
+            if (item.lowercased() == name.lowercased() && c == countOcc){
+                // valigia di cui prendere il peso
+                
+                for k in i...inputArray.count-1 {
+                    if inputArray[k].lowercased() == "kg" {
+                        
+                        if(inputArray[k-1] ~= "[0-9]{2}"){
+                            // il peso è scritto a numero
+                            return Int(inputArray[k-1])!
+
+                        }
+                    }
+                }
+                
+            }
+
+            i = i + 1
+            
+        }
         
-        // se non è presente alcun numero restituisco un valore di default in base alla categoria della valigia
-        switch category {
-            case "trolley":
-                peso = 10
-            case "bagaglio da stiva":
-                peso = 23
-            case "piccola":
-                peso = 10
-            case "zaino":
-                peso = 6
-            default:
-                // altro
-                peso = 10
+        if peso == 0{
+            // se non è presente alcun numero restituisco un valore di default in base alla categoria della valigia
+            switch category {
+                case "trolley":
+                    peso = 10
+                case "bagaglio da stiva":
+                    peso = 23
+                case "piccola":
+                    peso = 10
+                case "zaino":
+                    peso = 6
+                default:
+                    // altro
+                    peso = 10
+            }
+            // Grazia aggiungi gli altri casi ->>>>>>>>>>>>>
         }
         
         return peso
+    }
+    
+    func contaOccorrenze (input: String, name: String) -> Int {
+        let inputArray = input.components(separatedBy: " ")
+        var conta = 0
+        for i in inputArray{
+            if i == name {
+                conta += 1
+            }
+        }
+        return conta
     }
     
     let numbers:[Int:String] = [1:"uno", 2:"due", 3:"tre", 4:"quattro", 5:"cinque",
@@ -171,5 +232,11 @@ extension String {
         guard let regex = try? NSRegularExpression(pattern: rhs) else { return false }
         let range = NSRange(location: 0, length: lhs.utf16.count)
         return regex.firstMatch(in: lhs, options: [], range: range) != nil
+    }
+}
+
+extension Dictionary where Value: Equatable {
+    func key(from value: Value) -> Key? {
+        return self.first(where: { $0.value == value })?.key
     }
 }
